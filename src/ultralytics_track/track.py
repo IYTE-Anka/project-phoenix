8b8b4ba2504fd72d12cc06af39e524cd74629c12
Deniz Işık <denizisik2004@gmail.com>
2024-02-25 16:17:23 +0300

    Update the model to anka_v1.pt

--- src/ultralytics_track/track.py
+++ src/ultralytics_track/track.py
@@ -3,9 +3,7 @@ import cv2
 import os
 
 # Load YOLOV8 Model
-
-# model_filename = 'yolov8n.pt'
-model_filename = 'anka_v0.pt'
+model_filename = 'anka_v1.pt'
 model_path = os.path.join(os.path.dirname(__file__), "models", model_filename)
 model = YOLO(model_path)
 

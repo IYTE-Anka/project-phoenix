c96aa079c930162c6bdf293d201e308173cd294b
Deniz Işık <denizisik2004@gmail.com>
2024-02-29 11:21:15 +0300

    Update val file location

--- src/ultralytics_track/track.py
+++ src/ultralytics_track/track.py
@@ -7,4 +7,6 @@ model_path = os.path.join(os.path.dirname(__file__), "models", model_filename)
 model = YOLO(model_path)
 
 source_path = os.path.join(os.path.dirname(__file__), "balloon_video.mp4") # Path to video, device id (int, usually 0 for built in webcams)
-results = model.track(source=source_path, show=True, persist=True, save=True, project=os.path.dirname(__file__)) # Save results to project folder
+# Show and save results to project folder
+results = model.track(source=source_path, show=True, persist=True, save=True, project=os.path.join(os.path.dirname(__file__), "val"))
+ 

22db5f865092914df1645adf33e2052e83edc880
Deniz Işık <denizisik2004@gmail.com>
2024-02-29 11:17:36 +0300

    Implement video saving

--- src/ultralytics_track/track.py
+++ src/ultralytics_track/track.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
-import cv2
 import os
 
 # Load YOLOV8 Model
@@ -8,4 +7,4 @@ model_path = os.path.join(os.path.dirname(__file__), "models", model_filename)
 model = YOLO(model_path)
 
 source_path = os.path.join(os.path.dirname(__file__), "balloon_video.mp4") # Path to video, device id (int, usually 0 for built in webcams)
-results = model.track(source=source_path, show=True)
+results = model.track(source=source_path, show=True, persist=True, save=True, project=os.path.dirname(__file__)) # Save results to project folder

df973151bebe0158a8db2540ce4cf3247c6cefb0
Deniz Işık <denizisik2004@gmail.com>
2024-02-29 11:38:54 +0300

    Replace deprecated method of tracking
    results = model.track(...) works but raises an soft-error and opens room for possible memory allocation problems

--- src/ultralytics_track/track.py
+++ src/ultralytics_track/track.py
@@ -7,6 +7,10 @@ model_path = os.path.join(os.path.dirname(__file__), "models", model_filename)
 model = YOLO(model_path)
 
 source_path = os.path.join(os.path.dirname(__file__), "balloon_video.mp4") # Path to video, device id (int, usually 0 for built in webcams)
+
 # Show and save results to project folder
-results = model.track(source=source_path, show=True, persist=True, save=True, project=os.path.join(os.path.dirname(__file__), "val"))
- 
+for r in model.track(source=source_path, show=True, stream=True, persist=True, save=True, project=os.path.join(os.path.dirname(__file__), "val")):
+    pass
+# DEPRECATED: results = model.track(source=source_path, show=True, persist=True, save=True, project=os.path.join(os.path.dirname(__file__), "val"))
+
+# TODO: Put points on the center of the bounding boxes

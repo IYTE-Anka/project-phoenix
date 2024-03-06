67e9b504677486761c8aa8cea37e2b4d26b79c34
Deniz Işık <denizisik2004@gmail.com>
2024-03-06 16:06:29 +0300

    Fixed #13

--- src/ultralytics_track/track_plot.py
+++ src/ultralytics_track/track_plot.py
@@ -52,28 +52,25 @@ def run(source_path, model_path, conf_threshold):
       # Run YOLOv8 tracking on the frame, persisting tracks between frames
       results = model.track(frame, persist=True, tracker="bytetrack.yaml", conf=conf_threshold)
 
-      if results[0].boxes is None or results[0].boxes.id is None: # Prevent the program from crashing when no object is detected
-        cv2.imshow("ANKA Balon Tespit", frame)
-        continue
-
-      # Get the boxes and track IDs
-      boxes = results[0].boxes.xywh.cpu()
-      track_ids = results[0].boxes.id.int().cpu().tolist()
-
       # Visualize the results on the frame
       annotated_frame = results[0].plot()
 
-      # Plot the tracks
-      for box, track_id in zip(boxes, track_ids):
-        x, y, w, h = box
-        track = track_history[track_id]
-        track.append((float(x), float(y)))  # x, y center point
-        if len(track) > 30:  # retain 90 tracks for 90 frames
-          track.pop(0)
+      if results[0].boxes is not None and results[0].boxes.id is not None: # Fixes issue#13 - Video stops in the output when there is no detection 
+        # Get the boxes and track IDs
+        boxes = results[0].boxes.xywh.cpu()
+        track_ids = results[0].boxes.id.int().cpu().tolist()
 
-        # Draw the tracking lines
-        points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-        cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+        # Plot the tracks
+        for box, track_id in zip(boxes, track_ids):
+          x, y, w, h = box
+          track = track_history[track_id]
+          track.append((float(x), float(y)))  # x, y center point
+          if len(track) > 30:  # retain 90 tracks for 90 frames
+            track.pop(0)
+
+          # Draw the tracking lines
+          points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+          cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
 
       hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
       # Set range for red color and  

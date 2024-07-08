c50ea516c6fa174e95ba326b6939eea524b7e01a
Deniz Işık <denizisik2004@gmail.com>
2024-07-08 17:31:47 +0300

    Only run color detection in mode 3

--- src/gui.py
+++ src/gui.py
@@ -75,51 +75,51 @@ def video_stream():
             cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
             cv2.circle(annotated_frame, (int(x), int(y)), 5, (0, 0, 0), -1) # Put circle on the center of the balloons bboxes
 
-        # SECTION: COLOR TRACKING
-        # Define the color ranges for red, green, and blue in HSV color space
-        color_ranges = {
-          "Kirmizi": [(136, 87, 111), (180, 255, 255)],
-          "Yesil": [(25, 52, 72), (102, 255, 25)],
-          "Mavi": [(94, 80, 2), (120, 255, 25)]
-        }
+        if current_mode == "Mod 3":
+          # SECTION: COLOR TRACKING
+          # Define the color ranges for red, green, and blue in HSV color space
+          color_ranges = {
+            "Kirmizi": [(136, 87, 111), (180, 255, 255)],
+            "Yesil": [(25, 52, 72), (102, 255, 25)],
+            "Mavi": [(94, 80, 2), (120, 255, 25)]
+          }
 
-        # Convert the frame to HSV color space
-        hsv_frame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        if boxes is not None:
-          for box, track_id in zip(boxes, track_ids):
-            x, y, w, h = box
-            # Extract the ROI from the HSV frame
-            roi = hsv_frame[int(y):int(y+h), int(x):int(x+w)]
-            for color_, (lower, upper) in color_ranges.items():
-              # Create a mask for the current color
-              mask = cv2.inRange(roi, lower, upper)
-              # If the color is found in the ROI, annotate it on the frame
-              if cv2.countNonZero(mask) > 0:
-                cv2.putText(annotated_frame, color_, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                break
+          # Convert the frame to HSV color space
+          hsv_frame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+          if boxes is not None:
+            for box, track_id in zip(boxes, track_ids):
+              x, y, w, h = box
+              # Extract the ROI from the HSV frame
+              roi = hsv_frame[int(y):int(y+h), int(x):int(x+w)]
+              for color_, (lower, upper) in color_ranges.items():
+                # Create a mask for the current color
+                mask = cv2.inRange(roi, lower, upper)
+                # If the color is found in the ROI, annotate it on the frame
+                if cv2.countNonZero(mask) > 0:
+                  cv2.putText(annotated_frame, color_, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                  break
 
         # Display FPS
         fps = "30" #FIXME: Temporary value
         cv2.putText(annotated_frame, fps, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
-        cv2.putText(annotated_frame, current_mode, (annotated_frame.shape[1] - 200, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(annotated_frame, current_mode, (annotated_frame.shape[1] - 100, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3, cv2.LINE_AA)
         # Convert the Image object into a TkPhoto object
         im = Image.fromarray(annotated_frame)
         img = ImageTk.PhotoImage(image=im)
         # Update the image_label with a new image
         image_label.config(image=img)
         image_label.image = img
+        
       else:
         fps = "30" #FIXME: Temporary value
         cv2.putText(image, fps, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
-        cv2.putText(image, current_mode, (image.shape[1] - 200, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(image, current_mode, (image.shape[1] - 100, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3, cv2.LINE_AA)
         # Display the frame
         im = Image.fromarray(image)
         img = ImageTk.PhotoImage(image=im)
         # Update the image_label with a new image
         image_label.config(image=img)
         image_label.image = img
-        
-      
   finally:
     connection.close()
     client_socket.close()

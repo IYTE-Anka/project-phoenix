ed2a1326e7ab8d2138b91527876f747d558b60b9
Deniz Işık <denizisik2004@gmail.com>
2024-07-24 17:07:55 +0300

    Added color information to the object details

--- src/gui.py
+++ src/gui.py
@@ -57,7 +57,7 @@ def video_stream():
       # Decode the image from the stream
       image = np.asarray(bytearray(image_stream.read()), dtype="uint8")
       image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-      # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) DEPRECATED: BGR to RGB conversion is not needed anymore
+      # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # DEPRECATED: BGR to RGB conversion is not needed anymore
       
       if current_mode in ["Mod 2", "Mod 3"]:
         # SECTION: OBJECT DETECTION
@@ -67,13 +67,13 @@ def video_stream():
         # Visualize the results on the frame
         annotated_frame = results[0].plot()
         boxes = None
+        objects = []
         
         # SECTION: TRAJECTORY PLOTTING
         if results[0].boxes is not None and results[0].boxes.id is not None: # Fixes Issue#13 - Video stops in the output when there is no detection 
           # Get the boxes and track IDs
           boxes = results[0].boxes.xywh.cpu()
           track_ids = results[0].boxes.id.int().cpu().tolist()
-          objects = []
             
           # Plot the tracks
           for box, track_id in zip(boxes, track_ids):
@@ -96,9 +96,6 @@ def video_stream():
             points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
             cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
             cv2.circle(annotated_frame, (int(x), int(y)), 5, (0, 0, 0), -1) # Put circle on the center of the balloons bboxes
-          
-          json_data = json.dumps(objects)
-          send_data(json_data)
 
         if current_mode == "Mod 3":
           # SECTION: COLOR TRACKING
@@ -122,7 +119,13 @@ def video_stream():
                 # If the color is found in the ROI, annotate it on the frame
                 if cv2.countNonZero(mask) > 0:
                   cv2.putText(annotated_frame, color_, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                  break
+                  for obj in objects:
+                    if obj['track_id'] == track_id:
+                      obj['color'] = color_
+                      break
+
+        json_data = json.dumps(objects)
+        send_data(json_data)
 
         # Display FPS
         fps = "30" #FIXME: Temporary value

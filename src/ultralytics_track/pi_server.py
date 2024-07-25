7198832875bb784cb85042c66dd865c5a23e9432
Deniz Işık <denizisik2004@gmail.com>
2024-07-25 19:35:17 +0300

    Update parameters

--- src/ultralytics_track/pi_server.py
+++ src/ultralytics_track/pi_server.py
@@ -18,14 +18,14 @@ cw_direction    = 0
 ccw_direction   = 1 
 
 # Define parameters
-total_steps = 100  # Total number of steps
-ramp_steps = 25    # Number of steps for ramp-up and ramp-down
+total_steps = 50  # Total number of steps
+ramp_steps = 15    # Number of steps for ramp-up and ramp-down
 constant_speed_steps = total_steps - 2 * ramp_steps  # Steps at constant speed
 
 # Define delay parameters
-initial_delay = 0.01  # Initial delay for ramp-up
-final_delay = 0.01 # Final delay for ramp-down
-constant_delay = 0.001  # Delay during constant speed
+initial_delay = 0.05  # Initial delay for ramp-up
+final_delay = 0.05  # Final delay for ramp-down
+constant_delay = 0.05  # Delay during constant speed
 
 
 gpio.setmode(gpio.BCM)
